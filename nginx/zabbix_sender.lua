92688480eab01c679ac21d146514e820ff94d36e
dannc <d.savosin@findevelop.ru>
2017-05-23 12:56:17 +0700

    add logging

--- nginx/zabbix_sender.lua
+++ nginx/zabbix_sender.lua
@@ -3,18 +3,21 @@ local cjson = require("cjson")
 ngx.req.read_body()
 
 local data = ngx.req.get_body_data()
+ngx.log(ngx.INFO, data)
 
 if data then
   local value = cjson.new().decode(data)
   if type(value) == 'table' then
     for k,v in ipairs(value) do
       for key, value in pairs(v) do
+        ngx.log(ngx.INFO, key .. " " .. value)
         local command = "zabbix_sender -z localhost -p 8080 -s \"host\" -k " .. key
           .. " -o " .. value
         local handle = io.popen(command)
         local result = handle:read("*a")
         handle:close()
-        ngx.print(result)
+        ngx.log(ngx.INFO, result)
+        ngx.say(result)
       end
     end
     ngx.exit(ngx.HTTP_OK)
